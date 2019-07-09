d76412ce987a9eec3ced2590a1123c5ad480b4da
Scott Reese <scott_reese@pm.me>
2019-07-09 11:37:45 -0400

    added helper package dir_search for finding files. Light integration in dirwatcher.py of that function

--- dirwatcher.py
+++ dirwatcher.py
@@ -5,6 +5,7 @@ import time
 
 from helper.signal_handler import signal_handler
 from helper.create_parser import create_parser
+from helper.dir_search import dir_search
 
 exit_flag = False
 
@@ -23,19 +24,23 @@ def main(args):
     # Hook these two signals from the OS ..
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
+    
+    __dir, __ext = parsed_args.dir, parsed_args.ext
+    polling_interval, magic = parsed_args.int, parsed_args.magic
+    init_files = dir_search(__ext, __dir)
     # Now my signal_handler will get called if OS sends either of these to my process.
 
-    while not exit_flag:
-        try:
-            pass
-           # call my directory watching function.
-        except Exception as e:
-            pass
-            # This is an UNHANDLED exception
-            # Log an ERROR level message here
+    # while not exit_flag:
+    #     try:
+    #         pass
+    #        # call my directory watching function.
+    #     except Exception as e:
+    #         pass
+    #         # This is an UNHANDLED exception
+    #         # Log an ERROR level message here
 
-            # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-        time.sleep(polling_interval)
+    #         # put a sleep inside my while loop so I don't peg the cpu usage at 100%
+    #     time.sleep(polling_interval)
 
     # final exit point happens here
     # Log a message that we are shutting down
---
+++ helper/dir_search.py
@@ -0,0 +1,20 @@
+import os
+
+__author__ = "Scott Reese"
+
+def dir_search(ext, file_path = './'):
+    """Searches for files based on path and extension. Returns list of files found.
+    
+    Arguments:
+        ext {string} -- extension of files to be searched
+    
+    Keyword Arguments:
+        file_path {str} -- file path to find files (default: {'./'})
+
+    Returns:
+        {list} -- list of filenames
+    """
+    try:
+        return [file for file in os.listdir(file_path) if file.endswith(ext)]
+    except OSError as e:
+        print(e)
